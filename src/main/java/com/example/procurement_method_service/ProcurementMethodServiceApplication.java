334571ddfc6a8c2f508560dccdf9ab5d5aa6315b
Asus <mr.akmal4555@mail.ru>
2022-08-19 16:19:40 +0500

    Initial commit

---
+++ src/main/java/com/example/procurement_method_service/ProcurementMethodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.procurement_method_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementMethodServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcurementMethodServiceApplication.class, args);
+    }
+
+}
